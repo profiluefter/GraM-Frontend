ca35e0665e6d38b1b63059aaa2df523c399e4a88
Fabian <fabian.paul.gurtner@gmail.com>
2020-07-28 03:10:36 +0200

    Fix GitHub Pages: 404 when using Router-URLs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootswatch/dist/darkly/bootstrap.min.css';
-import './style.css'
 
 import Login from './Routes/Login';
 import Dashboard from './Routes/Dashboard';
